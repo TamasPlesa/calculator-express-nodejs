0b358b92fc502d8fa54cae0ee5c257c7ce8342dd
TamasPlesa <tamasplesa@icloud.com>
2023-04-05 00:19:48 +0200

    Created BMI calculator with logic, get, and post method

--- calculator.js
+++ calculator.js
@@ -8,6 +8,10 @@ app.get("/", (req, res)=> {
     res.sendFile(__dirname + "/index.html")
 })
 
+app.get("/bmicalculator", (req, res) => {
+    res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
 app.post("/", (req, res) => {
 
     let num1 = Number(req.body.num1)
@@ -16,6 +20,17 @@ app.post("/", (req, res) => {
     let result = num1 + num2
     
     res.send("The result of the calculation is " + result)
+})
+
+app.post("/bmicalculator", (req, res) => {
+
+    let weight = Number(req.body.weight);
+    let height = Number(req.body.height);
+
+    let bmi = weight / (height**2);
+
+
+    res.send("Your BMI is " + bmi)
 })
 
 app.listen(3000, ()=> {
